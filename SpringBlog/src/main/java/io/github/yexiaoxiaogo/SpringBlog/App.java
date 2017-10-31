e24cc8af45e3d3c4110992c2e41d8f6462029964
yexiaoxiaogo <841079936@qq.com>
2017-10-31 23:30:13 +0800

    start-dubbo整合spring boot未完

--- SpringBlog/src/main/java/io/github/yexiaoxiaogo/SpringBlog/App.java
+++ SpringBlog/src/main/java/io/github/yexiaoxiaogo/SpringBlog/App.java
@@ -5,8 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import io.github.yexiaoxiaogo.weather.service.WeatherCityService;
-
 /**
  * Hello world!
  *
@@ -14,12 +12,9 @@ import io.github.yexiaoxiaogo.weather.service.WeatherCityService;
 @SpringBootApplication
 @MapperScan("io.github.yexiaoxiaogo.SpringBlog.dao")
 public class App {
-	public static void main(String[] args) throws Exception {
-		SpringApplication.run(App.class, args);
-		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
-				new String[] { "META-INF/spring/consumer.xml" });
-		context.start();
-		WeatherCityService weatherCityService = (WeatherCityService) context.getBean("weatherCityService"); // 获取远程服务代理
-
+	public static ClassPathXmlApplicationContext context;
+	public static void main(String[] args) {
+		context = (ClassPathXmlApplicationContext) SpringApplication.run(App.class, args);
+			
 	}
 }
